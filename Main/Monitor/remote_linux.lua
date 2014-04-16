a5ff8953b9004ba2c4628516fd609c196ad1cf09
Scott Tomaszewski <scottTomaszewski@gmail.com>
2014-04-16 11:33:00 -0400

    Removes unnecessary metadata

--- Main/Monitor/remote_linux.lua
+++ Main/Monitor/remote_linux.lua
@@ -1,14 +1,5 @@
--- Metadata
-meta.id = "Koka.Monitor"
-meta.author = "Koka"
-meta.name = "Monitor"
-meta.description = "Monitor management"
-meta.layouts = "Monitor.layouts"
-meta.icon = "Monitor.png"
-
 local task = libs.task;
 
--- Action
 actions.turn_on = function ()
   task.start("xset", "dpms", "force", "on");
 end
