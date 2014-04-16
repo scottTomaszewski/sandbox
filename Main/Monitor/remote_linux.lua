80b01c0f6664317b5097820869f073c536aba4f1
Scott Tomaszewski <scottTomaszewski@gmail.com>
2014-04-16 11:33:44 -0400

    Adds basic documentation

--- Main/Monitor/remote_linux.lua
+++ Main/Monitor/remote_linux.lua
@@ -1,9 +1,11 @@
 local task = libs.task;
 
+--@help Wake up monitor
 actions.turn_on = function ()
   task.start("xset", "dpms", "force", "on");
 end
 
+--@help Put monitor to sleep
 actions.turn_off = function ()
   task.start("xset", "dpms", "force", "off");
 end
